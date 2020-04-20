c5c4d3106bf3756767409919f60b26d6718270de
Nicolas Ménant <menant.nicolas@gmail.com>
2020-04-20 12:13:09 +0200

    fix AS3 declaration for Azure

--- Azure/AS3_Declaration/main.tf
+++ Azure/AS3_Declaration/main.tf
@@ -19,10 +19,10 @@ provider "bigip" {
   address     = var.f5_public_ip
   port        = var.f5_mgmt_port
   username    = var.f5_admin_user
-  password    = data.azurerm_key_vault_secret.bigip_admin_password.value
+  password    = var.f5_admin_password
 }
 
 resource "bigip_as3"  "as3-base-declaration" {
   as3_json    = "${file(var.as3_filename)}"
-  config_name = var.f5_config_name
+  tenant_name = var.f5_config_name
  }
